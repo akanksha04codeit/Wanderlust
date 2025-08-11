034fb93754dcd39f2672c5c9ee01cde7da2dc96e
Akanksha Pawashe <akankshapawashe@gmail.com>
2025-08-11 18:37:45 +0530

    Redirect to accessed path after login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js")
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -30,11 +31,12 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 })
 
-router.post("/login",
+router.post("/login", saveRedirectUrl,
     passport.authenticate("local",{failureRedirect: '/login',failureFlash: true}),
     async(req,res)=>{
     req.flash("success","Welcome back to Wanderlust!");
-    res.redirect("/listing");
+    let redirectUrl = res.locals.redirectUrl || "/listing"
+    res.redirect(redirectUrl);
 })
 
 router.get("/logout", (req, res, next) =>{
